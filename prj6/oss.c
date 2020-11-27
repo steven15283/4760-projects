2e2424246b15265135541946660d5e2ed3be7ede
steven <stevenguo15283@gmail.com>
2020-11-27 17:23:04 -0600

    Update oss.c

--- prj6/oss.c
+++ prj6/oss.c
@@ -111,7 +111,8 @@ void oss(simtime_t* simClock, int maxActive)
             // check page table for frame
             int framePos = pageTables[msg.pid].pages[msg.address / 1024].framePos;
             int action = msg.action;//read(1) or write(2) or terminate(3)
-            if (action == TERMINATE) {
+            if (action == TERMINATE) 
+            {
                 int i;
                 // get real pid of terminating process and set to available
                 int pid = pids[msg.pid];
@@ -259,4 +260,297 @@ void oss(simtime_t* simClock, int maxActive)
     // Delete message queue
     msgctl(msqid, IPC_RMID, NULL);
     return;
+}
+
+
+
+
+
+
+// Initialize the array of pids and memory management structures
+void init_mem_management(int* pids, frame_t* frameTable, pagetable_t* pageTables, int maxActive)
+{
+    int i, j;
+    // Initialize all pids to available ( = -1)
+    for (i = 0; i < maxActive; i++)
+    {
+        pids[i] = -1;
+    }
+    // Initialize frame table
+    for (i = 0; i < TOTAL_MEMORY; i++)
+    {
+        frameTable[i].ref = 0x0;
+        frameTable[i].dirty = 0x0;
+        frameTable[i].pid = -1;//indicates frame is empty
+    }
+    // Initialize page tables
+    for (i = 0; i < maxActive; i++)
+    {
+        for (j = 0; j < PROCESS_SIZE; j++)
+        {
+            pageTables[i].pages[j].framePos = -1;
+        }
+    }
+}
+
+//Handle command line arguments using getopt
+void handle_args(int argc, char* argv[], int* maxActive)
+{
+    int opt;
+    char* logName = "log.txt";
+    if (argc < 2)
+    {
+        printf("No arguments given\n");
+        printf("Using default values\n");
+    }
+    while ((opt = getopt(argc, argv, "hn:o:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            printf("This program takes the following possible arguments\n");
+            printf("\n");
+            printf("  -h           : to display this help message\n");
+            printf("  -n x         : x = maximum concurrent child processes\n");
+            printf("  -o filename  : to specify log file\n");
+            printf("\n");
+            printf("Defaults:\n");
+            printf("  Log File: log.txt\n");
+            printf("         n: 18\n");
+            printf("  Non-verbose printing\n");
+            exit(EXIT_SUCCESS);
+        case 'n':
+            *maxActive = atoi(optarg);
+            break;
+        case 'o':
+            logName = optarg;
+            break;
+        default:
+            printf("No arguments given\n");
+            printf("Using default values\n");
+            break;
+        }
+    }
+    if (*maxActive > 18)
+    {
+        printf("n must be <= 18\n");
+        exit(EXIT_SUCCESS);
+    }
+    else if (*maxActive <= 0)
+    {
+        printf("n must be >= 1\n");
+        exit(EXIT_SUCCESS);
+    }
+    printf("Log File: %s\n", logName);
+    printf("       n: %d\n", *maxActive);
+    // Open log file
+    logFile = open_file(logName, "w", "./oss: Error: ");
+    return;
+}
+
+//SIGALRM handler
+static void time_out()
+{
+    fprintf(stderr, "Timeout\n");
+    cleanup();
+    exit(EXIT_SUCCESS);
+}
+
+//clean up for abnormal termination
+void cleanup()
+{
+    fclose(logFile);
+    shmctl(simClockID, IPC_RMID, NULL);
+    msgctl(msqid, IPC_RMID, NULL);
+    kill(0, SIGTERM);
+    exit(EXIT_SUCCESS);
+}
+
+/*fopen with simple error check*/
+FILE* open_file(char* fname, char* opts, char* error)
+{
+    FILE* fp = fopen(fname, opts);
+    if (fp == NULL) {  // error opening file
+        perror(error);
+        cleanup();
+    }
+    return fp;
+}
+
+//return a pointer to simtime_t object in shared memory
+simtime_t* get_shared_simtime(int key)
+{
+    simtime_t* simClock;
+    simClockID = shmget(key, sizeof(simtime_t), IPC_CREAT | 0777);
+    if (simClockID < 0)
+    {
+        perror("./oss: Error: shmget for simulated clock");
+        cleanup();
+    }
+    simClock = shmat(simClockID, NULL, 0);
+    if (simClock < 0)
+    {
+        perror("./oss: Error: shmat for simulated clock");
+        cleanup();
+    }
+    return simClock;
+}
+
+//set the msqid
+void create_msqueue()
+{
+    msqid = msgget(MSG_Q_KEY, 0666 | IPC_CREAT);
+    if (msqid < 0) {
+        perror("./oss: Error: msgget ");
+        cleanup();
+    }
+    return;
+}
+
+//return index of free spot in pids array, -1 if no spots
+int get_simPid(int* pids, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        if (pids[i] == -1)
+        {
+            return i;
+        }
+    return -1;
+}
+
+//spawn a process with the args and increment running process counter
+//essentially just fork() and execl()
+int spawn(char arg1[], char arg2[], int* active)
+{
+    int pid = fork();
+    if (pid < 0)
+    {  // error
+        perror("./oss: Error: fork ");
+        cleanup();
+    }
+    else if (pid == 0)
+    {  // child
+        execl("./user", "user", arg1, arg2, (char*)NULL);
+    }
+    (*active)++;
+    return pid;
+}
+
+//Get a random time that the next process should spawn
+//returns a simClock + random time between 0 and given max simtime
+simtime_t get_next_process_time(simtime_t max, simtime_t currentTime)
+{
+    simtime_t nextTime = { .ns = (rand() % (max.ns + 1)) + currentTime.ns,.s = (rand() % (max.s + 1)) + currentTime.s };
+    if (nextTime.ns >= 1000000000)
+    {
+        nextTime.s += 1;
+        nextTime.ns -= 1000000000;
+    }
+    return nextTime;
+}
+
+//only two important parts of an oss to child message...
+//the destination (pid of receiving process)
+//and the action sent to them (denied or granted)
+void send_msg(int dest, enum actions action)
+{
+    msg_t msg = { .mtype = dest,
+                  .pid = -1,//child doesn't need to know
+                  .address = -1,//child already know what it asked for
+                  .action = action
+    };
+    if (msgsnd(msqid, &msg, sizeof(msg_t), 0) == -1)
+    {
+        perror("./oss: Error: msgsnd ");
+        cleanup();
+    }
+    return;
+}
+
+// returns the position of the first empty frame in the table. -1 if their are no empty frames
+int get_empty_frame_pos(frame_t* table, int frames)
+{
+    int i;
+    for (i = 0; i < frames; i++)
+    {
+        if (table[i].pid == -1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+// Return the index of the least recently used frame
+// - linear search through the frame table and storing the index of the frame
+// with the lowest reference that we have seen
+int get_lru_frame_pos(frame_t* table, int frames)
+{
+    int i;
+    int lru_frame = 0; // Least recently used frame index
+    for (i = 0; i < frames; i++)
+    {
+        // If frame is occupied and has a lower reference byte the the previous lowest
+        // then set lowest frame index to that frame
+        if (table[i].pid > 0 && table[i].ref < table[lru_frame].ref)
+            lru_frame = i;
+    }
+    return lru_frame;
+}
+
+// Bit shift the reference bytes of all the occupied frames down by 1
+void shift_refBytes(frame_t* table, int frames)
+{
+    int i;
+    for (i = 0; i < frames; i++)
+    {
+        table[i].ref = table[i].ref >> 1;//shift bits down by 1 bit
+    }
+}
+
+// Print the formatted frame table
+void print_frame_table(frame_t* table, int frames, simtime_t now, FILE* out)
+{
+    int i;
+    fprintf(out, "Current memory layout at time %d:%9d is:\n", now.s, now.ns);
+    fprintf(out, "           Occupied  RefByte  DirtyBit\n");
+    for (i = 0; i < frames; i++)
+    {
+        if (table[i].pid >= 0) // > 0 indicates occupied frame
+            fprintf(out, "Frame %3d: %-9s %-8d %-8d\n", i, "Yes", table[i].ref, table[i].dirty);
+        else
+            fprintf(out, "Frame %3d: %-9s %-8d %-8d\n", i, "No", table[i].ref, table[i].dirty);
+    }
+    return;
+}
+
+void print_stats(simtime_t* simClock, int faults, int references, FILE* out)
+{
+    double totalTimeInSeconds = (double)(simClock->s) + ((double)(simClock->ns) / 1000000000);
+    double refsPerSecond = (double)references / totalTimeInSeconds;
+    double faultsPerReference = (double)faults / (double)references;
+    fprintf(out, "References per second: %lf\n", refsPerSecond);
+    fprintf(out, "Faults per Reference: %lf\n", faultsPerReference);
+    return;
+}
+
+int main(int argc, char* argv[])
+{
+    int maxActive = 18;
+    simtime_t* simClock;
+    //Seed rand
+    srand(time(0));
+    //Two second time out
+    signal(SIGALRM, time_out);
+    alarm(2);
+    //Handle command line arguments
+    handle_args(argc, argv, &maxActive);
+    //Set up shared simulated clock
+    simClock = get_shared_simtime(SIM_CLOCK_KEY);
+    simClock->s = 0;
+    simClock->ns = 0;
+
+    oss(simClock, maxActive);
+
+    return 0;
 }
