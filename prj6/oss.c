cff4778d6139f226d78a3ab01767731b4b7c2adb
steven <stevenguo15283@gmail.com>
2020-11-29 17:00:42 -0600

    Update oss.c

--- prj6/oss.c
+++ prj6/oss.c
@@ -40,47 +40,43 @@ void print_stats(simtime_t* simClock, int faults, int references, FILE* out);
 
 void oss(simtime_t* simClock, int maxActive)
 {
-
-    /*****SETUP*****/
     msg_t msg;//holds the current message being sent or last recieved
     int lines = 0;//counter of lines written to the log
     int activeProcesses = 0;//counter of active processes 
     simtime_t nextSpawn = { .s = 0, .ns = 0 };//simulated time for next spawn
     int spawnInc = 1000000; // 1ms = 1000000 ns
-
     int noPageFaultInc = 10;
     int pageFaultInc = 1000;
     int readWriteInc = 15;
     int referenceCounter = 0;//counter of total frame references
     int pageFaultCounter = 0;//counter of total page faults
-    //Array of taken sim pids. index = simpid, value at index = actual pid
+    //array of taken sim pids. index = simpid, value at index = actual pid
     int* pids = (int*)malloc(sizeof(int) * maxActive);
-    // Frame Table. array of frames
-    frame_t* frameTable = (frame_t*)malloc(sizeof(frame_t) * TOTAL_MEMORY);
-    // Array of page tables. one page table for each process
+    frame_t* frameTable = (frame_t*)malloc(sizeof(frame_t) * TOTAL_MEMORY);// frame Table. array of frames
+    //array of page tables. one page table for each process
     pagetable_t* pageTables = (pagetable_t*)malloc(sizeof(pagetable_t) * maxActive);
-    // Initialize the array of pids and memory management structures
+    // initialize the array of pids and memory management structures
     init_mem_management(pids, frameTable, pageTables, maxActive);
     // Set up the message queue
     create_msqueue();
 
 
-    /*****OSS*****/
+    /oss
     fprintf(logFile, "Begin OS Simulation\n");
     lines++;
     while (1)
     {
-        //If oss should spawn a new child process
+        //if oss should spawn a new child process
         if (activeProcesses < maxActive && less_or_equal_sim_times(nextSpawn, *simClock) == 1)
         {
-            //Spawn
+            //spawn
             int simPid;//simulated pid
             int pid;//real child pid
-            char msqidArg[10];//exec argument
-            char simPidArg[3];//exec argument
+            char msqidArg[10];//exec arg
+            char simPidArg[3];//exec arg
             simPid = get_simPid(pids, maxActive);
             if (simPid < 0)
-            {  //Error
+            {  //error
                 fprintf(logFile, "%d.%09ds ./oss: Error: No available simPids\n", simClock->s, simClock->ns);
                 fprintf(stderr, "./oss: Error: No available simPids\n");
                 cleanup();
@@ -96,13 +92,13 @@ void oss(simtime_t* simClock, int maxActive)
             //note it is suggested to spawn every 1 - 500ms but this would not spawn enough children fast enough
             //this is reduced to spawn children more frequently
             nextSpawn = get_next_process_time((simtime_t) { 0, 500000 }, (*simClock));
-            // Logging
+            // logging
             if (lines++ < 100000)
                 fprintf(logFile, "%d.%09ds ./oss: Spawned new process p%d\n", simClock->s, simClock->ns, simPid);
             // Increment the simulated clock
             increment_sim_time(simClock, spawnInc);
         }
-        /** MEMORY MANAGEMENT **/
+        //memory management
         // Check the message queue for memory requests or termination
         else if ((msgrcv(msqid, &msg, sizeof(msg_t), ossChannel, IPC_NOWAIT)) > 0)
         {
@@ -132,7 +128,7 @@ void oss(simtime_t* simClock, int maxActive)
                 }
                 activeProcesses--;
                 waitpid(pid, NULL, 0);
-                // Logging
+                // logging
                 if (lines++ < 100000)
                 {
                     fprintf(logFile, "%d.%09ds ./oss: p%d terminated\n", simClock->s, simClock->ns, msg.pid);
@@ -142,7 +138,7 @@ void oss(simtime_t* simClock, int maxActive)
             {
                 if (action == WRITE)
                 {
-                    // Logging
+                    // logging
                     if (lines++ < 100000)
                     {
                         fprintf(logFile, "%d.%09ds ./oss: p%d requests WRITE at Address:%d\n", simClock->s, simClock->ns, msg.pid, msg.address);
@@ -150,45 +146,41 @@ void oss(simtime_t* simClock, int maxActive)
                 }
                 else
                 {
-                    // Logging
+                    // logging
                     if (lines++ < 100000)
                     {
                         fprintf(logFile, "%d.%09ds ./oss: p%d requests READ at Address:%d\n", simClock->s, simClock->ns, msg.pid, msg.address);
                     }
                 }
-                //printf("%d\t%d == %d\n", framePos, frameTable[framePos].pid, msg.pid);
-                /** NO PAGE FAULT **/
+                //no page fault
                 if (framePos != -1 && frameTable[framePos].pid == msg.pid)
                 {
-                    // Logging
+                    // logging
                     if (lines++ < 100000)
                     {
                         fprintf(logFile, "%d.%09ds ./oss: GRANTED p%d Address:%d in Frame:%d\n", simClock->s, simClock->ns, msg.pid, msg.address, framePos);
                     }
-                    //send_msg(msg.pid, GRANTED);
-                    //set most sig bit with bitwise or
-                    //ex 00101011 | 10000000 = 10101011 (0x80 = 128 = 10000000)
+                    
+                    //set most sig bit with bitwise or ex 00101011 | 10000000 = 10101011 (0x80 = 128 = 10000000)
                     frameTable[framePos].ref = frameTable[framePos].ref | 0x80;
-                    //if it s\is a write then set dirty bit
+                    //if it is a write then set dirty bit
                     if (action == WRITE)
                     {
                         frameTable[framePos].dirty = 0x1;
-                        // Logging
+                        // logging
                         if (lines++ < 100000)
                         {
                             fprintf(logFile, "%d.%09ds ./oss: Setting dirty bit. Frame: %d\n", simClock->s, simClock->ns, framePos);
                         }
                     }
-                    // Increment the simulated clock
+                    // increment the simulated clock
                     increment_sim_time(simClock, noPageFaultInc);
                 }
-                /** END of NO PAGE FAULT **/
-                /** PAGE FAULT **/
                 else
                 {
                     pageFaultCounter++;
                     int framePos = get_empty_frame_pos(frameTable, TOTAL_MEMORY);
-                    /** PAGE REPLACEMENT **/
+                    //page replacement
                     if (framePos == -1)
                     {
                         // get the position of the frame to replace
@@ -203,16 +195,14 @@ void oss(simtime_t* simClock, int maxActive)
                         //update page table
                         pageTables[msg.pid].pages[msg.address / 1024].framePos = framePos;
                     }
-                    /** END of PAGE REPLACEMENT **/
-                    /** PAGE INSERT **/
                     else
                     {
-                        // Logging
+                        //logging
                         if (lines++ < 100000)
                         {
                             fprintf(logFile, "%d.%09ds ./oss: Inserting Page into Frame: %d\n", simClock->s, simClock->ns, framePos);
                         }
-                        // Set the frame
+                        //set the frame
                         frameTable[framePos].pid = msg.pid;
                         frameTable[framePos].ref = 0x80;
                         if (action == WRITE)
@@ -224,13 +214,9 @@ void oss(simtime_t* simClock, int maxActive)
                     }
                     //send_msg(msg.pid, GRANTED);
                     increment_sim_time(simClock, pageFaultInc);
-                    /** END of PAGE INSERT **/
                 }
-                /** END of PAGE FAULT **/
             }
         }
-
-        /** END of MEMORY MANAGEMENT **/
         // shift ref bytes, print memory map, and print statistics
         if ((referenceCounter + 1) % 100 == 0)
         {
@@ -246,18 +232,11 @@ void oss(simtime_t* simClock, int maxActive)
 
 
     }
-
-    // Should never get here in final version but clean up just in case
-    // // Free pids
-    // free(pids);
-    // // Free tables 
-    // free(frameTable);
-    // free(pageTables);
-    // Close log
+    //close log
     fclose(logFile);
-    // Delete sim clock
+    //delete sim clock
     shmctl(simClockID, IPC_RMID, NULL);
-    // Delete message queue
+    //delete message queue
     msgctl(msqid, IPC_RMID, NULL);
     return;
 }
@@ -267,23 +246,23 @@ void oss(simtime_t* simClock, int maxActive)
 
 
 
-// Initialize the array of pids and memory management structures
+//initialize the array of pids and memory management structures
 void init_mem_management(int* pids, frame_t* frameTable, pagetable_t* pageTables, int maxActive)
 {
     int i, j;
-    // Initialize all pids to available ( = -1)
+    //initialize all pids to available ( = -1)
     for (i = 0; i < maxActive; i++)
     {
         pids[i] = -1;
     }
-    // Initialize frame table
+    //initialize frame table
     for (i = 0; i < TOTAL_MEMORY; i++)
     {
         frameTable[i].ref = 0x0;
         frameTable[i].dirty = 0x0;
         frameTable[i].pid = -1;//indicates frame is empty
     }
-    // Initialize page tables
+    //initialize page tables
     for (i = 0; i < maxActive; i++)
     {
         for (j = 0; j < PROCESS_SIZE; j++)
@@ -293,7 +272,7 @@ void init_mem_management(int* pids, frame_t* frameTable, pagetable_t* pageTables
     }
 }
 
-//Handle command line arguments using getopt
+//handle command line arguments using getopt
 void handle_args(int argc, char* argv[], int* maxActive)
 {
     int opt;
@@ -343,7 +322,7 @@ void handle_args(int argc, char* argv[], int* maxActive)
     }
     printf("Log File: %s\n", logName);
     printf("       n: %d\n", *maxActive);
-    // Open log file
+    //open log file
     logFile = open_file(logName, "w", "./oss: Error: ");
     return;
 }
@@ -366,7 +345,7 @@ void cleanup()
     exit(EXIT_SUCCESS);
 }
 
-/*fopen with simple error check*/
+//fopen with simple error check
 FILE* open_file(char* fname, char* opts, char* error)
 {
     FILE* fp = fopen(fname, opts);
@@ -437,7 +416,7 @@ int spawn(char arg1[], char arg2[], int* active)
     return pid;
 }
 
-//Get a random time that the next process should spawn
+//get a random time that the next process should spawn
 //returns a simClock + random time between 0 and given max simtime
 simtime_t get_next_process_time(simtime_t max, simtime_t currentTime)
 {
@@ -481,9 +460,9 @@ int get_empty_frame_pos(frame_t* table, int frames)
     }
     return -1;
 }
-// Return the index of the least recently used frame
-// - linear search through the frame table and storing the index of the frame
-// with the lowest reference that we have seen
+//return the index of the least recently used frame
+//- linear search through the frame table and storing the index of the frame
+//with the lowest reference that we have seen
 int get_lru_frame_pos(frame_t* table, int frames)
 {
     int i;
@@ -498,7 +477,7 @@ int get_lru_frame_pos(frame_t* table, int frames)
     return lru_frame;
 }
 
-// Bit shift the reference bytes of all the occupied frames down by 1
+//bit shift the reference bytes of all the occupied frames down by 1
 void shift_refBytes(frame_t* table, int frames)
 {
     int i;
@@ -508,7 +487,7 @@ void shift_refBytes(frame_t* table, int frames)
     }
 }
 
-// Print the formatted frame table
+//print the formatted frame table
 void print_frame_table(frame_t* table, int frames, simtime_t now, FILE* out)
 {
     int i;
@@ -538,14 +517,14 @@ int main(int argc, char* argv[])
 {
     int maxActive = 18;
     simtime_t* simClock;
-    //Seed rand
+    //seed rand
     srand(time(0));
-    //Two second time out
+    //two second time out
     signal(SIGALRM, time_out);
     alarm(2);
-    //Handle command line arguments
+    //handle command line arguments
     handle_args(argc, argv, &maxActive);
-    //Set up shared simulated clock
+    //set up shared simulated clock
     simClock = get_shared_simtime(SIM_CLOCK_KEY);
     simClock->s = 0;
     simClock->ns = 0;
